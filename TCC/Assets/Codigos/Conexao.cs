03b22290f5a3f263f96a2df8dbe1e24834e2c310
Eduardo2712 <52054453+Eduardo2712@users.noreply.github.com>
2020-10-29 00:36:36 -0300

    Atualização 29/10/2020

---
+++ TCC/Assets/Codigos/Conexao.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using SocketIO;
+
+public class Conexao : MonoBehaviour
+{
+    public static Conexao instancia;
+    public SocketIOComponent socket;
+
+    void Start()
+    {
+        if (instancia == null)
+        {
+            instancia = this;
+            socket = GetComponent<SocketIOComponent>();
+            socket.On("PONG", OnReceivePong);
+        }
+        else
+        {
+            Destroy(this.gameObject);
+        }
+    }
+
+    public void OnReceivePong(SocketIOEvent pack)
+    {
+        Dictionary<string, string> result = pack.data.ToDictionary();
+        Debug.Log("mensagem do servidor: " + result["message"]);
+    }
+
+    public void SendPingToServer()
+    {
+        Dictionary<string, string> pack = new Dictionary<string, string>();
+        pack["message"] = "Deu certo!!!";
+        socket.Emit("PING", new JSONObject(pack));
+    }
+
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.E))
+        {
+            SendPingToServer();
+        }
+    }
+}
