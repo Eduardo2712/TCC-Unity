b5f0afbe2e64f2499a25feed212ede2f87a42c4e
Eduardo2712 <52054453+Eduardo2712@users.noreply.github.com>
2020-11-09 00:39:45 -0300

    Atualização 09/11/2020

---
+++ TCC/Assets/Codigos/MovimentaChao.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovimentaChao : MonoBehaviour
+{
+    public GameObject chao;
+    private Vector3 localProxChao;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        localProxChao.x = 249;
+        StartCoroutine(CriaChao());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+    }
+
+    IEnumerator CriaChao()
+    {
+        yield return new WaitForSeconds(1);
+        GameObject aux = Instantiate(chao, localProxChao, chao.transform.rotation);
+        localProxChao.x += 249;
+        Destroy(aux, 10f);
+        StartCoroutine(CriaChao());
+    }
+}
